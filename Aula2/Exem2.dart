831edbe733c43d2f50a8e394f5f8c642256ec6d5
LucasSantino <lsantino541@gmail.com>
2025-02-03 20:28:11 -0300

    update

--- Aula2/Exem2.dart
+++ Aula2/Exem2.dart
@@ -6,7 +6,7 @@ print("Digite seu nome"); // Exibe a mensagem solicitando que o usario digite o 
 // Digite o seu nome
 // null safety
 // só ira converter o dado se a entrada não for vazia
-String nome =stdin.readLineSync()!;
+String nome =stdin.readLineSync()!; // input dos dados
 print("Digite sua idade");
 String idade =stdin.readLineSync()!;
 
--- Aula2/Exem3.dart
+++ Aula2/Exem3.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 
 void main(){ // Função Principal ///
   print("Digite o primeiro numero");
-  
+
   // int. parse faz a conversao
-  var num1 = stdin.readLineSync()!;
+  var num1 = stdin.readLineSync()!; // input dos dados
   print("Digite o segundo numero");
   var num2 = stdin.readLineSync()!;
 int  res = int.parse(num1)+ int.parse(num2);
