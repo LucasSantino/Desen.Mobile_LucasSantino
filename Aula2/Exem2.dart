e9f8be76de13a06227ace68b4e428b3e3cebf321
LucasSantino <lsantino541@gmail.com>
2025-02-03 20:10:08 -0300

    Exemplo 2

---
+++ Aula2/Exem2.dart
@@ -0,0 +1,12 @@
+// importa a biblioteca dart:io
+import 'dart:io'; // Permite que o usario entrar com dados
+
+void main(){  /// Função Principal
+print("Digite seu nome"); // Exibe a mensagem solicitando que o usario digite o seu nome
+// Digite o seu nome
+// null safety
+// só ira converter o dado se a entrada não for vazia
+String nome =stdin.readLineSync()!;
+
+
+}
