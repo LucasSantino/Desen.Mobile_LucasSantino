b8a0664a6f0168622a32442969b1c79b4b870416
LucasSantino <lsantino541@gmail.com>
2025-02-03 19:53:57 -0300

    Update Exem1.dart

--- Aula2/Exem1.dart
+++ Aula2/Exem1.dart
@@ -4,4 +4,15 @@ import 'dart:io';
 void main(){
   // declarando a variavel do tipo inteira
   // chamada idade
+  int idade = 29;
+  print("Idade: $idade");
+
+  // declarando variavel do tipo double
+  double raio = 10.25;
+  print("Raio: $raio");
 }
+
+// Declarando variavel do tipo String
+
+String nome = "Daniel ";
+
