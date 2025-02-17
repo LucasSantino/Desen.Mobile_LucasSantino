c9f2c06dd27e4556d9d2f606c233b6c9bf0ab3dc
LucasSantino <lsantino541@gmail.com>
2025-02-17 19:44:07 -0300

    Create exer3.dart

---
+++ Aula3/exercicios/exer3.dart
@@ -0,0 +1,3 @@
+/*3. Crie uma função que calcule o salário líquido do usuário a partir dos valores digitados pelo teclado considerando um desconto de 10% de
+impostos e bonificação de 20% em cima do salário.*/
+
