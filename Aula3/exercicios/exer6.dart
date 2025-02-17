96a128473c8d1cb9b136cf64a656e8dcf3260839
LucasSantino <lsantino541@gmail.com>
2025-02-17 20:19:52 -0300

    exercicio 5 - finalizado

---
+++ Aula3/exercicios/exer6.dart
@@ -0,0 +1,21 @@
+/*6. Crie um programa que receba a idade de duas pessoas e print na tela qual é a pessoa mais velha.*/
+
+import 'dart:io';
+
+void main() {
+  // Recebe as idades das duas pessoas
+  stdout.write('Digite a idade da primeira pessoa: ');
+  int idade1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Digite a idade da segunda pessoa: ');
+  int idade2 = int.parse(stdin.readLineSync()!);
+
+  // Compara as idades e exibe quem é a pessoa mais velha
+  if (idade1 > idade2) {
+    print('A primeira pessoa é a mais velha.');
+  } else if (idade2 > idade1) {
+    print('A segunda pessoa é a mais velha.');
+  } else {
+    print('As duas pessoas têm a mesma idade.');
+  }
+}
