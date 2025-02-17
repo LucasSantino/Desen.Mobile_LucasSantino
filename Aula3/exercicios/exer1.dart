236b2fb6d4130f2f7211b7dd769c671cd0d8ee40
LucasSantino <lsantino541@gmail.com>
2025-02-17 19:34:02 -0300

    Exercicio 1 - Finalizado

---
+++ Aula3/exercicios/exer1.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void receberInformacoes() {
+  // Receber o nome do usuário
+  stdout.write('Digite o seu nome: ');
+  String nome = stdin.readLineSync()!;
+
+  // Receber o curso do usuário
+  stdout.write('Digite o seu curso: ');
+  String curso = stdin.readLineSync()!;
+
+  // Receber a idade do usuário
+  stdout.write('Digite a sua idade: ');
+  int idade = int.parse(stdin.readLineSync()!);
+
+  // Exibir as informações recebidas
+  print('\nInformações do Usuário:');
+  print('Nome: $nome');
+  print('Curso: $curso');
+  print('Idade: $idade anos');
+}
+
+void main() {
+  receberInformacoes();
+}
