ea4cf5fb3216627fde71e7fd4e013401131de07d
LucasSantino <lsantino541@gmail.com>
2025-02-24 19:38:26 -0300

    Update exemplo1.dart

--- Aula5/exemplo1.dart
+++ Aula5/exemplo1.dart
@@ -1,24 +1,25 @@
 /*
-Exemplo 1 - Programação oreintada a objeto 24.02.2025
+Exemplo 1 - Programaçao orientada a objeto
+24.02.2025
 */
 
-// Cria a classe chamada casa
-class Casa {
-  //String cor= "Branco"; // Inicializa a variavel com o valor padrão
-  String? cor; //faz parte do null safety do dart
+
+// Cria classe chamada Casa
+class Casa{
+  String? cor; // ? faz parte do null safety do dart
   int? qtde_p;
 
-  /// Cria o metodo chamado abrirporta
-  void abrirporta() {
+  // Cria o metodo chamado abrirporta()
+  void abrirporta(){
     print("A porta esta aberta");
   }
 }
 
-void main() {
-  // instancia o  objeto chamado chamado minhaCasa
-  Casa minhaCasa = Casa();
-  minhaCasa.cor = "Azul";
-  minhaCasa.qtde_p = 2;
+void main(){
+  // instancia o objeto chamado minhaCasa
+  Casa  minhaCasa = Casa();
+  minhaCasa.cor="Azul";
+  minhaCasa.qtde_p=2;
   minhaCasa.abrirporta();
   print("Cor da casa ${minhaCasa.cor}");
   print("Quantidade de portas: ${minhaCasa.qtde_p}");
