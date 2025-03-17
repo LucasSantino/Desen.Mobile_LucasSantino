c4214759bb40ba73e4c693749b1978c889f57b5e
Lucas Santino <lsantino541@gmail.com>
2025-03-17 19:07:48 -0300

    Update exemplo5.dart

--- Aula6/exemplo5.dart
+++ Aula6/exemplo5.dart
@@ -1,91 +1,92 @@
 /*
-Correçao exercicio 4 aula 05
+Correção exercício 4 aula 05
 Criar uma classe produtos com alguns atributos conforme enunciado
 10.03.2025
 */
 
-// Cria a classe produtos
-class Produtos{
+// Cria a classe Produtos
+class Produtos {
   String nome;
   int qtde;
   double preco;
   String tipo_comun;
   double consumo_kw;
 
-  // cria o construtor
-  Produtos(this.nome, this.qtde,this.preco,this.tipo_comun,this.consumo_kw);
+  // Cria o construtor
+  Produtos(this.nome, this.qtde, this.preco, this.tipo_comun, this.consumo_kw);
 
-  // cria metodo para ligar o produto
-
-  void ligar(){
+  // Cria método para ligar o produto
+  void ligar() {
     print("Produto ligado");
   }
 }
 
-// Classe chamada fritadeira
+// Classe chamada Fritadeira
+class Fritadeira extends Produtos {
+  // Construtor herdando parâmetros da classe pai
+  Fritadeira(String nome, int qtde, double preco, String tipo_comun, double consumo_kw)
+      : super(nome, qtde, preco, tipo_comun, consumo_kw);
 
-class Fritadeira extends Produtos{
-  // construtor herdando parametros da classe pai
-
-  Fritadeira(nome,qtde,preco, tipo_com,consumo_kw):super(nome,qtde, preco,tipo_com,consumo_kw);
-
-
-  //polimorfismo Override
+  // Polimorfismo Override
   @override
-  void ligar(){
+  void ligar() {
     print("Fritadeira ${nome} ligada");
   }
-  void desligar(){
+
+  void desligar() {
     print("Fritadeira ${nome} desligada");
   }
 
-  void ajustatemp(int temp){
-    int setpoint =250;
-    if(temp>=250){
-      print("Temperatura ajustada");
-    }else{
-
-      while(temp<250){
-        temp = temp+10;
-        print("Temperatura: ${temp}");
+  void ajustatemp(int temp) {
+    // Usando a variável setpoint para controlar a temperatura
+    int setpoint = 250;
+    if (temp >= setpoint) {
+      print("Temperatura ajustada para $setpoint°C");
+    } else {
+      while (temp < setpoint) {
+        temp = temp + 10;
+        print("Ajustando temperatura: $temp°C");
       }
-      print("Temperatura ajustada: ${temp}");
+      print("Temperatura ajustada para $setpoint°C");
     }
-
   }
 }
 
-class ArCondicionado extends Produtos{
-  ArCondicionado(nome,qtde,preco,tipo_com,consumo):super(nome,qtde,preco,tipo_com,consumo);
+// Classe chamada ArCondicionado
+class ArCondicionado extends Produtos {
+  ArCondicionado(String nome, int qtde, double preco, String tipo_comun, double consumo_kw)
+      : super(nome, qtde, preco, tipo_comun, consumo_kw);
+
   @override
-  void ligar(){
+  void ligar() {
     print("Ar condicionado ligado");
   }
 
-  void desligar(){
+  void desligar() {
     print("Ar condicionado desligado");
   }
 
-  void ajustatemp(int temp){
-    int setpoint =22;
-    if(temp==setpoint){
-      print("Temperatura ok");
-    }
-    else{
-      while(temp<setpoint){
-        temp = temp+2;
-        print("Ajuste de temperatura $temp");
+  void ajustatemp(int temp) {
+    // Usando a variável setpoint para controlar a temperatura
+    int setpoint = 22;
+    if (temp == setpoint) {
+      print("Temperatura já está ajustada para $setpoint°C");
+    } else {
+      while (temp < setpoint) {
+        temp = temp + 2;
+        print("Ajustando temperatura: $temp°C");
       }
-      print("Temperatura ok");
-    }   
-  }}
-
-  void main(){
-    Fritadeira Philips = Fritadeira("Philips", 1, 800.0, "Wifi", 1.4);
-    Philips.ligar();
-    Philips.ajustatemp(100);
-    ArCondicionado LG = ArCondicionado("LG", 1, 3500.0, "Bluetooth",3.5);
-    LG.ligar();
-    LG.ajustatemp(0);
-
+      print("Temperatura ajustada para $setpoint°C");
+    }
   }
+}
+
+void main() {
+  Fritadeira Philips = Fritadeira("Philips", 1, 800.0, "Wifi", 1.4);
+  Philips.ligar();
+  Philips.ajustatemp(100);
+
+  ArCondicionado LG = ArCondicionado("LG", 1, 3500.0, "Bluetooth", 3.5);
+  LG.ligar();
+  LG.ajustatemp(0);
+}
