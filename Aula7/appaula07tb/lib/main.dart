8d4b7260212df4419f392a1825ffb27d37922644
Lucas Santino <lsantino541@gmail.com>
2025-03-17 20:00:36 -0300

    update

--- Aula7/appaula07tb/lib/main.dart
+++ Aula7/appaula07tb/lib/main.dart
@@ -1,28 +1,55 @@
-// biblioteca material que vai permitir a criação dos widgetspara o Flutter no android
+// biblioteca material que vai permitir a criaçao dos widgets para o Flutter no android
 import 'package:flutter/material.dart';
 
-
-// Função principal do app
+// void main funçao principal do app
 void main() {
-  // runApp função qie constroi o app chamado a classe do tipo Stateless
-  runApp(const Telapp());
+  // runApp funçao que constroi o app chamando a classe do tipo Stateless
+  runApp(Telapp());
 }
 
 class Telapp extends StatelessWidget {
   const Telapp({super.key});
 
-// overwrite é o polimorfismo, permite tratar os objetos de diferentes formas
+// override polimorfismo, permite tratar objetos de diferentes formas
   @override
   Widget build(BuildContext context) {
     // MaterialApp faz parte do design do aplicativo
     return MaterialApp(
       home: Column(
-        // Alinhamento primario
-        //mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-
+        // alinhamento primario
+        // mainAxisAlignment: MainAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.end,
+        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(width: 300, height: 100,color: Colors.blue),
+          Container(
+            width: 300,height: 100,color: Colors.blue,
+          ),
+          Container(
+            width: 300,height: 100,color: Colors.red,
+            child: Text("Senai - Mobile",
+            style: TextStyle(fontSize: 30,color: Colors.white,decoration: TextDecoration.none),
+            ),
+          ),
+
+          Stack(
+            // propriedade de alinhamento
+            alignment: Alignment.center,
+        children: [
+              Container(width: 300,height: 100,color: Colors.blue,),
+              Container(width: 200,height: 80,color: Colors.red,
+              child: Text("Lucas",textAlign: TextAlign.center,
+              style: TextStyle(
+                    color: Colors.white,decoration: TextDecoration.none,fontSize: 25,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Text permite inserir texto no aplicativo
+        
         ],
       ),
     );
