7345a96905419c4adc09679053c5bc35b0e10af4
Lucas Santino <lsantino541@gmail.com>
2025-03-17 20:05:08 -0300

    Update main.dart

--- Aula7/appaula07tb/lib/main.dart
+++ Aula7/appaula07tb/lib/main.dart
@@ -17,38 +17,37 @@ class Telapp extends StatelessWidget {
     return MaterialApp(
       home: Column(
         // alinhamento primario
-        // mainAxisAlignment: MainAxisAlignment.center,
+       // mainAxisAlignment: MainAxisAlignment.center,
         //mainAxisAlignment: MainAxisAlignment.end,
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       //mainAxisAlignment: MainAxisAlignment.spaceAround,
+       //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            width: 300,height: 100,color: Colors.blue,
-          ),
-          Container(
-            width: 300,height: 100,color: Colors.red,
-            child: Text("Senai - Mobile",
-            style: TextStyle(fontSize: 30,color: Colors.white,decoration: TextDecoration.none),
-            ),
-          ),
+        Container(width: 300, height: 100,color: Colors.blue,),
+        Container(width: 300,height: 100,color: Colors.red,child: 
+        Text("Senai - Mobile",style: TextStyle(fontSize: 30,
+        color: Colors.white,
+        decoration: TextDecoration.none),
+        ),),
 
-          Stack(
-            // propriedade de alinhamento
-            alignment: Alignment.center,
+        Stack(
+          // propriedade de alinhamento
+          alignment: Alignment.center ,
         children: [
-              Container(width: 300,height: 100,color: Colors.blue,),
-              Container(width: 200,height: 80,color: Colors.red,
-              child: Text("Lucas",textAlign: TextAlign.center,
-              style: TextStyle(
-                    color: Colors.white,decoration: TextDecoration.none,fontSize: 25,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          //Text permite inserir texto no aplicativo
+          Container(width: 300,height: 100,color: Colors.blue,),
+          Container(width: 200,height: 80,color: Colors.red,
+          child: Text("Daniel",textAlign: TextAlign.center,          
+          style: TextStyle(
+            color: Colors.white,
+            decoration: TextDecoration.none,
+            fontSize: 25,
+
+            
+            ),),),
+        ],
+        ),
+        //Text permite inserir texto no aplicativo
         
         ],
       ),
